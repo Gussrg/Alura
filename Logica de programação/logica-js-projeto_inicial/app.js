b371bf9717465e3c20541afbfe48f7c1d848c3ea
Gustavo Rebelo <134798105+Gussrg@users.noreply.github.com>
2023-12-26 19:54:17 -0300

    Update app.js

--- Logica de programação/logica-js-projeto_inicial/app.js
+++ Logica de programação/logica-js-projeto_inicial/app.js
@@ -1,3 +1,11 @@
-alert('Olá mundo!');
-let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+alert('Jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
+
+//se chute for igual ao numero secreto
+if(numeroSecreto == chute) {
+    console.log('Você descobriu o número secreto (5)');
+} else {
+    alert('Você errou');
+}
