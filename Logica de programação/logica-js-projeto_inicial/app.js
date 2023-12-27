75d078955613797d736e9503d108db0616e70ca5
Gustavo Rebelo <134798105+Gussrg@users.noreply.github.com>
2023-12-27 15:18:22 -0300

    Update app.js

--- Logica de programação/logica-js-projeto_inicial/app.js
+++ Logica de programação/logica-js-projeto_inicial/app.js
@@ -1,15 +1,15 @@
 alert('Jogo do número secreto');
-let numeroSecreto = 6;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto o chute não for igual ao numero secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     //se chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O numero secreto é menor que ${chute}`);
@@ -20,3 +20,11 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+/*if (tentativas > 1) {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}*/
