964967924fd49c0750ef1ceb3b187c80ee66ed03
Gustavo Rebelo <134798105+Gussrg@users.noreply.github.com>
2023-12-28 07:26:45 -0300

    Aula java

---
+++ Java/primeiroProjeto/src/main/java/alura/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package alura.primeiroprojeto;
+
+/**
+ *
+ * @author gusta
+ */
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+
+    }
+}
