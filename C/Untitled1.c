8ec1f7e81ef55c1109676e7acb737860ebde7eb1
Gustavo Rebelo <gustavo.rblo@gmail.com>
2024-01-14 17:02:57 -0300

    Aulas de C
    Introdução a C

---
+++ C/Untitled1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("*******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *!\n");
+    printf("*******************************************\n");
+
+}
