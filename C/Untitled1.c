b5a59018d42bc42b3a8efe4a2a3c441905bea12b
Gustavo Rebelo <134798105+Gussrg@users.noreply.github.com>
2024-01-15 19:22:44 -0300

    Aula 2 de C

--- C/Untitled1.c
+++ C/Untitled1.c
@@ -1,8 +1,39 @@
 #include<stdio.h>
 
-int main(){
+int main(void)
+{
+
     printf("*******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação *!\n");
     printf("*******************************************\n");
 
+    int numeroSecreto = 42, chute;
+
+    for(int i = 1; i <=3; i++)
+    {
+        printf("tentativa %d de 3\n", i);
+        printf("Qual é o seu chute?\n");
+        scanf("%d",&chute);
+        if(chute == numeroSecreto)
+        {
+            printf("Parabens!Você acertou!\n");
+            printf("Jogue de novo!\n");
+            break;
+        }
+        else
+        {
+            if(chute > numeroSecreto)
+            {
+                printf("Seu número é maior que o número secreto!\n");
+            }
+            if(chute < numeroSecreto)
+            {
+                printf("Seu número é menor que o numero secreto\n");
+            }
+        }
+    }
+
+    printf("Fim de jogo!\n");
+
+    return(0);
 }
